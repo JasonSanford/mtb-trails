7705bd89f60740de9770924b2b9b32bf9788398d
Jason Sanford <jasonsanford@gmail.com>
2016-02-02 09:27:47 -0500

    Remove gulpfile

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var jsonminify = require('gulp-jsonminify');
-
-gulp.task('default', function() {
-  return gulp.src('./trails/src/*.geojson')
-    .pipe(jsonminify())
-    .pipe(gulp.dest('./trails/dist'));
-});
