8fbb86b6a97d126d09aaed75363540882b4e397d
Jason Sanford <jasonsanford@gmail.com>
2016-10-18 09:10:05 -0400

    Add beatty. Build.

--- get_trails.sh
+++ get_trails.sh
@@ -3,3 +3,6 @@ echo '[out:json][timeout:25];(way["highway"="cycleway"](35.654506,-80.948209,35.
 
 # US National Whitewater Center
 echo '[out:json][timeout:25];(way["highway"="cycleway"](35.257779,-81.013956,35.283602,-80.991897););out body;>;out skel qt;' | ./node_modules/query-overpass/cli.js --flat-properties > trails/us_national_whitewater_center.geojson
+
+# Colonel Francis Beatty Park
+echo '[out:json][timeout:25];(way["highway"="cycleway"](35.055645,-80.752945,35.071839,-80.730543););out body;>;out skel qt;' | ./node_modules/query-overpass/cli.js --flat-properties > trails/beatty.geojson
